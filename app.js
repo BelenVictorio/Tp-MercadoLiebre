5475e4826e8985180770412871bb47c197197a16
Belén Victorio <victoriobell@gmail.com>
2022-04-30 12:01:40 -0300

    register y login terminado

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     return res.sendFile(path.join(__dirname, "views", "home.html"))
 });
+app.get("/register", (req, res) => {
+    return res.sendFile(path.join(__dirname, "views", "register.html"))
+});
+app.get("/login", (req, res) => {
+    return res.sendFile(path.join(__dirname, "views", "login.html"))
+});
 
 
 app.listen(port, () => console.log(`Servidor funcionando en el puerto http://localhost:${port}`));
